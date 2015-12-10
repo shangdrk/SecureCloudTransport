4f8cf30c0d85258b71bf40a30ab5e2506a7dda38
student <student>
2015-12-10 22:56:29 +0100

    Need to merge

--- src/hu/ait/crypto/Main.java
+++ src/hu/ait/crypto/Main.java
@@ -141,19 +141,5 @@ public class Main {
                 System.out.println(USAGE);
             }
         }
-
-
-
-
-
-
-//        if (args.length > 0) {
-//            try {
-//                firstArg = Integer.parseInt(args[0]);
-//            } catch (NumberFormatException e) {
-//                System.err.println("Argument" + args[0] + " must be an integer.");
-//                System.exit(1);
-//            }
-//        }
     }
 }
